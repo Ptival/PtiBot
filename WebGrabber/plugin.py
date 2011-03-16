c1b8fa913a93555f8bafdecbaaa60cad25ebd540
Valentin Robert <valentin.robert.42@gmail.com>
2011-03-16 04:16:07 -0700

    Added WebGrabber

---
+++ WebGrabber/plugin.py
@@ -0,0 +1,77 @@
+###
+# Copyright (c) 2011, Valentin Robert
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#   * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions, and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions, and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#   * Neither the name of the author of this software nor the name of
+#     contributors to this software may be used to endorse or promote products
+#     derived from this software without specific prior written consent.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+###
+
+import string
+import urllib2
+from BeautifulSoup import BeautifulSoup
+
+import supybot.utils as utils
+from supybot.commands import *
+import supybot.plugins as plugins
+import supybot.ircutils as ircutils
+import supybot.callbacks as callbacks
+
+
+class WebGrabber(callbacks.Plugin):
+    """Grabs random stuff from the internetz!"""
+    threaded = True
+
+    def urbandic(self, irc, msg, args):
+        """- Grabs a random Urban Dictionary definition."""
+        url = 'http://www.urbandictionary.com/random.php'
+        try:
+            website = urllib2.urlopen(url)
+        except urllib2.HTTPError, e:
+            irc.reply('A problem occured. Please try again.')
+            return
+        soup = BeautifulSoup(website,
+                             convertEntities=BeautifulSoup.HTML_ENTITIES)
+        td_word = soup.findAll(name='td',
+                               attrs={'class':'word'},
+                               limit=1)
+        div_def = soup.findAll(name='div',
+                               attrs={'class':'definition'},
+                               limit=1)
+        for t in td_word:
+            word = string.replace(t.string, '\n', '')
+            irc.reply('Word: ' + word, prefixNick=False)
+        defn = ''
+        for d in div_def:
+            for c in d.contents:
+                if c.string:
+                    defn += c.string
+        irc.reply('Def.: ' + defn, prefixNick=False)
+
+    urbandic = wrap(urbandic)
+
+Class = WebGrabber
+
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
